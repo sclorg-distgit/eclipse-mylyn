27662b1e0a32f53a89110a29a4382d078f528428
Honza Horak <hhorak@redhat.com>
2016-01-06 14:04:50 +0100

    Initial import

---
+++ fetch-eclipse-mylyn-incubator.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+RELEASE_TAG="07a5ce39847b8dc5921180942db31d30f9d2d4f8"
+FETCHED_SOURCES_NAME="eclipse-mylyn-${RELEASE_TAG}-incubator-fetched-src"
+
+#clean up old runs
+rm -rf $FETCHED_SOURCES_NAME
+
+#checkout
+git clone git://git.eclipse.org/gitroot/mylyn/org.eclipse.mylyn.incubator.git $FETCHED_SOURCES_NAME
+cd $FETCHED_SOURCES_NAME
+git checkout $RELEASE_TAG
+
+#remove projects we do not need
+ls -1 | grep -v  -e "org.eclipse.mylyn.trac.wiki-feature" \
+    -e "org.eclipse.mylyn.trac.wiki" \
+    -e "org.eclipse.mylyn.web.tasks-feature" \
+    -e "org.eclipse.mylyn.web.tasks" | xargs rm -rf
+
+#remove precomipled binaries
+find . -type f -name ".class" -exec rm {} \;
+find . -type f -name ".jar" -exec rm {} \;
+rm -rf .git
+find . -type f -name ".gitignore" -exec rm {} \;
+find . -type f -name ".gitmodules" -exec rm {} \;
+
+cd ..
+
+#package and clean up
+tar -caf $FETCHED_SOURCES_NAME.tar.xz $FETCHED_SOURCES_NAME
+rm -rf $FETCHED_SOURCES_NAME
---
+++ fetch-eclipse-mylyn.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+RELEASE_TAG="R_3_16_0"
+FETCHED_SOURCES_NAME="eclipse-mylyn-${RELEASE_TAG}-fetched-src"
+
+#clean up old runs
+rm -rf $FETCHED_SOURCES_NAME
+
+#checkout
+git clone --recursive git://git.eclipse.org/gitroot/mylyn/org.eclipse.mylyn.all.git $FETCHED_SOURCES_NAME
+cd $FETCHED_SOURCES_NAME
+git checkout $RELEASE_TAG
+git submodule update
+
+#remove precomipled binaries
+find . -type f -name ".class" -exec rm {} \;
+find . -type f -name ".jar" -exec rm {} \;
+rm -rf .git
+find . -type f -name ".gitignore" -exec rm {} \;
+find . -type f -name ".gitmodules" -exec rm {} \;
+
+cd ..
+
+#package and clean up
+tar -caf $FETCHED_SOURCES_NAME.tar.xz $FETCHED_SOURCES_NAME
+rm -rf $FETCHED_SOURCES_NAME
